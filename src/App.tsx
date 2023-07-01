e17561ec445486b8fdc775f875098dd8a0be2bc5
Meerim K <thvrim@gmail.com>
2023-07-01 17:16:59 +0600

    импортировала главный контейнер

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Main from "./containers/Main/Main";
+import "./App.css";
 
 const App = () => {
   return (
       <div>
-        
+          <Main />
       </div>
   );
 };
